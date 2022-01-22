ed0e0d1dbc85c770b4e04106a7137b0ad8a37ce0
Jhon Avila <javilaortiz@gmail.com>
2022-01-22 02:49:50 -0500

    style: updating topbar style

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 
-import { Nav, NavItem, NavLink } from 'reactstrap'
+import Mars from './mars.png'
+
+import "./styles.scss";
 
 export function Topbar() {
   return (
-      <Nav>
-        <NavItem>
-          <NavLink
-            disabled
-            href="#"
-          >
-            Mars Rover Dashboard
-          </NavLink>
-        </NavItem>
-      </Nav>
+      <nav className="topbar">
+        <div className="brand">
+          <img className="brand-logo" width="24" src={Mars} alt="mars rovers icon"/>
+          <span className="brand-name">
+            Mars Rovers | Dashboard
+          </span>
+        </div>
+        <div className="search">
+          Search
+        </div>
+        <div className="user">
+          User
+        </div>
+      </nav>
   )
 }
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -8,45 +8,12 @@ import {
   Gallery
 } from "../../components"
 
-import { Row } from "reactstrap"
 import "./styles.scss"
 
 export function Dashboard() {
   return (
     <div className="bg-dark dashboard">
-      <div className="topbar">
-        <Topbar />
-      </div>
-      <div className="components">
-        <div className="sidebar">
-          <Sidebar />
-        </div>
-        <div className="content">
-          <div className="secondaryheader">
-            <div>name</div>
-            <div>audio tag</div>
-          </div>
-          <div className="currentLocation">
-            <CurrentLocation />          
-          </div>
-          <div className="draggableComponents">
-            <div className="component videofeed">
-              <Draggable>
-                <div className="box">
-                  <VideoFeed width="100%" height="auto" />
-                </div>
-              </Draggable>
-            </div>
-            <div className="component gallery">
-              <Draggable>
-                <div className="box">
-                  <Gallery width="100%" height="auto" />
-                </div>
-              </Draggable>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Topbar />
     </div>
   )
 }
