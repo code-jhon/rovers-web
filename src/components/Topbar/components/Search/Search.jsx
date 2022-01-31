b137e55fe74d31120a5a8db7635e7e1a7082e558
Jhon Avila <javilaortiz@gmail.com>
2022-01-31 02:57:14 -0500

    feat: automplete style and states

--- src/components/Topbar/components/Search/Search.jsx
+++ src/components/Topbar/components/Search/Search.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import "./styles.scss"
 
-export function Search() {
+function AutoComplete ({rovers, search}) {
+  return (
+    <div className='autoComplete'>
+      autocomplete
+    </div>
+  )
+}
+
+export function Search({rovers}) {
+  const [visible, setVisible] = useState(false)
+  const [search, setSearch] = useState('')
+
+
   return (
     <div>
       <input 
         type="text" 
         name="search" 
         className='searchInp' 
-        autocomplete="off" 
+        autoComplete="off"
+        placeHolder="Search"
+        onClick={e => setVisible(!visible)}
+        onChange={e => setSearch(e.target.value)}
       />
+      { visible && <AutoComplete rovers={rovers} search={search} /> }
     </div>
   );
 }
