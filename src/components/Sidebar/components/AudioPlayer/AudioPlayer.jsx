74a8bb38ac3629177db076ac9a6135de65695c9d
Jhon Avila <javilaortiz@gmail.com>
2022-01-30 22:34:41 -0500

    feat: adding specs component and fixing audio player dimentions

--- src/components/Sidebar/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/Sidebar/components/AudioPlayer/AudioPlayer.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 
+import "./styles.scss"
+
 export function AudioPlayer() {
   return (
-    <div>
+    <div className='audioplayer'>
       <ReactPlayer 
         className='react-player'
         loop
         controls 
         url="https://upload.wikimedia.org/wikipedia/commons/d/da/Sounds_of_Perseverance_Mars_rover_driving.oga" 
         width="100%"
-        height="100px"
+        height="27px"
       />
     </div>
   )
