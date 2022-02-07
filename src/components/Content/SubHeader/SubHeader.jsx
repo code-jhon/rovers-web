fc8163b83c4203734afa74fb2c2f36d10e453e5a
Jhon Avila <javilaortiz@gmail.com>
2022-02-07 01:49:01 -0500

    feat: subheader data from api

--- src/components/Content/SubHeader/SubHeader.jsx
+++ src/components/Content/SubHeader/SubHeader.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./styles.scss"
 
-export function SubHeader() {
+export function SubHeader({name, owner, manufacturer, specifications}) {
   return (
     <div className="subheader">
       <div className="rover">
-        Perseverance
-        <span className="carrier">Carrier</span>
+        {name}
+        <span className="carrier">{owner.name}</span>
       </div>
-      <div className="manufacturer">Manufacturer</div>
-      <div className="launched">Launched Date</div>
+      <div className="manufacturer">{manufacturer.name}</div>
+      <div className="launched">{specifications.rocket.launched}</div>
     </div>
   )
 }
